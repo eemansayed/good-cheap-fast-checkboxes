d1287c40e7968dc27b086ec537829830edfd2599
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-24 11:33:59 +0000

    add functionc called doTheTrick

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const fast = document.querySelector("#fast");
 toggles.forEach((toggle) =>
   toggle.addEventListener("change", (e) => doTheTrick(e.target))
 );
+
+function doTheTrick(theClickedOne) {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) {
+      fast.checked = false;
+    }
+    if (cheap === theClickedOne) {
+      good.checked = false;
+    }
+
+    if (fast === theClickedOne) {
+      cheap.checked = false;
+    }
+  }
+}
